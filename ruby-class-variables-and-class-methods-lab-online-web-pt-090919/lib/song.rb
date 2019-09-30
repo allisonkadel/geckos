18e4fcd9d28ad880447fe9c4eafd16891026fec2
allisonkadel <akadel01@gmail.com>
2019-09-30 15:35:13 -0400

    Add class variables and methods lab code

---
+++ ruby-class-variables-and-class-methods-lab-online-web-pt-090919/lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+    attr_reader :name, :genre, :artist
+    # attr_writer :name
+    # attr_accessor :name
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    # def name
+    #     @name
+    # end
+
+    # def genre
+    #     @genre
+    # end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def artist
+    #     @artist
+    # end
+
+end
